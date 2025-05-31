ae4668457093dadf464c4e6b6e7b11039c3b35a3
Antoine <antoine.bnrd2@gmail.com>
2025-05-31 18:39:42 +0200

    New features to control each tracks independently with sliders, and to button to go either straight forward or backward

--- lib/screens/control_screen.dart
+++ lib/screens/control_screen.dart
@@ -9,42 +9,146 @@ class ControlScreen extends StatefulWidget {
 }
 
 class _ControlScreenState extends State<ControlScreen> {
+  int leftTrack = 0;
+  int rightTrack = 0;
+
+  void setBothTracks(int value) {
+    setState(() {
+      leftTrack = value;
+      rightTrack = value;
+    });
+    // send to tank
+  }
+
+  void sendToTank() {
+    // Replace this with your Bluetooth or Serial communication
+    print("Left: $leftTrack, Right: $rightTrack");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-          colors: <Color>[
-            Color(0xff214c91),
-            Color(0xff154494),
-            Color(0xff093c96),
-            Color(0xff033297),
-            Color(0xff062896),
-            Color(0xff111b95),
-            Color(0xff1d149b),
-            Color(0xff2a16aa),
-            Color(0xff3717b9),
-            Color(0xff4317c8),
-            Color(0xff5117d7),
-            Color(0xff5e15e6),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            colors: <Color>[
+              Color(0xff214c91),
+              Color(0xff154494),
+              Color(0xff093c96),
+              Color(0xff033297),
+              Color(0xff062896),
+              Color(0xff111b95),
+              Color(0xff1d149b),
+              Color(0xff2a16aa),
+              Color(0xff3717b9),
+              Color(0xff4317c8),
+              Color(0xff5117d7),
+              Color(0xff5e15e6),
+            ],
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            const SizedBox(
+              width: 40,
+            ),
+            RotatedBox(
+              quarterTurns: -1,
+              child: Slider(
+                value: rightTrack.toDouble(),
+                max: 14,
+                divisions: 14,
+                label: "$rightTrack",
+                onChanged: (value) {
+                  setState(() => rightTrack = value.toInt());
+                },
+              ),
+            ),
+            SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      print("FORWARD !!!!!");
+                    },
+                    child: const SizedBox(
+                      width: 100,
+                      height: 120,
+                      child: Center(
+                        child: Text("FORWARD"),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("BACKWARD !!!!!");
+                    },
+                    child: const SizedBox(
+                      width: 100,
+                      height: 120,
+                      child: Center(
+                        child: Text("BACKWARD"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              child: const Text("barrel commands"),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (_) => const FiringScreen()));
+              },
+            ),
+            SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      print("TURN LEFT !!!!!");
+                    },
+                    child: const SizedBox(
+                      width: 100,
+                      height: 120,
+                      child: Center(
+                        child: Text("TURN LEFT"),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("TURN RIGHT !!!!!");
+                    },
+                    child: const SizedBox(
+                      width: 100,
+                      height: 120,
+                      child: Center(
+                        child: Text("TURN RIGHT"),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            RotatedBox(
+              quarterTurns: -1,
+              child: Slider(
+                value: leftTrack.toDouble(),
+                max: 14,
+                divisions: 14,
+                label: "$leftTrack",
+                onChanged: (value) {
+                  setState(() => leftTrack = value.toInt());
+                },
+              ),
+            ),
           ],
         ),
-      ),
-      child: Row(
-        children: [
-          const SizedBox(
-            width: 50,
-          ),
-          ElevatedButton(
-            child: const Text("barrel commands"),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => const FiringScreen()));
-            },
-          )
-        ],
       ),
     );
   }
