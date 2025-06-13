b696e63c9359ade3078b6c64dc543e118a91a48c
Antoine <antoine.bnrd2@gmail.com>
2025-06-13 16:04:18 +0200

    nothing changed

--- lib/screens/firing_screen.dart
+++ lib/screens/firing_screen.dart
@@ -3,6 +3,7 @@ import 'package:tank_app/screens/control_screen.dart';
 
 class FiringScreen extends StatefulWidget {
   const FiringScreen({super.key});
+
   @override
   State<FiringScreen> createState() => _FiringScreenState();
 }
@@ -19,10 +20,8 @@ class _FiringScreenState extends State<FiringScreen> {
           ElevatedButton(
             child: const Text("tank control"),
             onPressed: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (_) => const ControlScreen())
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (_) => const ControlScreen()));
             },
           )
         ],
