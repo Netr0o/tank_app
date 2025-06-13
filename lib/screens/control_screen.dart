0ae387b233812b24e7120105f8b6e44a439dc515
Antoine <antoine.bnrd2@gmail.com>
2025-06-13 16:00:10 +0200

    added of bluetooth features

--- lib/screens/control_screen.dart
+++ lib/screens/control_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tank_app/screens/firing_screen.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class ControlScreen extends StatefulWidget {
   const ControlScreen({super.key});
@@ -9,6 +10,59 @@ class ControlScreen extends StatefulWidget {
 }
 
 class _ControlScreenState extends State<ControlScreen> {
+  BluetoothDevice? _device;
+  BluetoothCharacteristic? _characteristic;
+
+  final String targetDeviceName = "MOUHAHA";
+  final Guid serviceUuid = Guid("19B10000-E8F2-537E-4F6C-D104768A1214");
+  final Guid characteristicUuid = Guid("19B10001-E8F2-537E-4F6C-D104768A1214");
+
+  @override
+  void initState() {
+    super.initState();
+    _startScan();
+  }
+
+  void _startScan() {
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5));
+
+    FlutterBluePlus.scanResults.listen((results) async {
+      for (ScanResult r in results) {
+        if (r.device.name == targetDeviceName) {
+          print("device: ${r.device.name}");
+          FlutterBluePlus.stopScan();
+          await _connectToDevice(r.device);
+        }
+      }
+    });
+  }
+
+  Future<void> _connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+    _device = device;
+
+    List<BluetoothService> services = await device.discoverServices();
+    for (var service in services) {
+      if (service.uuid == serviceUuid) {
+        for (var characteristic in service.characteristics) {
+          if (characteristic.uuid == characteristicUuid) {
+            _characteristic = characteristic;
+            print("found !!!!!");
+            setState(() {});
+            return;
+          }
+        }
+      }
+    }
+  }
+
+  void _sendOne() async {
+    if (_characteristic != null) {
+      await _characteristic!.write([0x01]);
+      print("send: 1");
+    }
+  }
+
   int leftTrack = 0;
   int rightTrack = 0;
 
@@ -56,14 +110,20 @@ class _ControlScreenState extends State<ControlScreen> {
             ),
             RotatedBox(
               quarterTurns: -1,
-              child: Slider(
-                value: rightTrack.toDouble(),
-                max: 14,
-                divisions: 14,
-                label: "$rightTrack",
-                onChanged: (value) {
-                  setState(() => rightTrack = value.toInt());
-                },
+              child: Center(
+                child: SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: Slider(
+                    value: rightTrack.toDouble(),
+                    max: 14,
+                    divisions: 14,
+                    label: "$rightTrack",
+                    onChanged: (value) {
+                      setState(() => rightTrack = value.toInt());
+                    },
+                  ),
+                ),
               ),
             ),
             SizedBox(
@@ -71,9 +131,7 @@ class _ControlScreenState extends State<ControlScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
-                    onPressed: () {
-                      print("FORWARD !!!!!");
-                    },
+                    onPressed: _characteristic != null ? _sendOne : null,
                     child: const SizedBox(
                       width: 100,
                       height: 120,
@@ -137,14 +195,20 @@ class _ControlScreenState extends State<ControlScreen> {
             ),
             RotatedBox(
               quarterTurns: -1,
-              child: Slider(
-                value: leftTrack.toDouble(),
-                max: 14,
-                divisions: 14,
-                label: "$leftTrack",
-                onChanged: (value) {
-                  setState(() => leftTrack = value.toInt());
-                },
+              child: Center(
+                child: SizedBox(
+                  width: 300,
+                  height: 40,
+                  child: Slider(
+                    value: leftTrack.toDouble(),
+                    max: 14,
+                    divisions: 14,
+                    label: "$leftTrack",
+                    onChanged: (value) {
+                      setState(() => leftTrack = value.toInt());
+                    },
+                  ),
+                ),
               ),
             ),
           ],
