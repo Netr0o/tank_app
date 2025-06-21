ecc0bf3d6247e7a5b9b7674262a2fb9ff9da5389
Antoine <antoine.bnrd2@gmail.com>
2025-06-21 19:57:18 +0200

    - total review of the theme : form light blue and white to black and orange - added new features : move back to the left/right

--- lib/screens/control_screen.dart
+++ lib/screens/control_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tank_app/screens/firing_screen.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ControlScreen extends StatefulWidget {
   const ControlScreen({super.key});
@@ -18,6 +19,11 @@ class _ControlScreenState extends State<ControlScreen> {
   final Guid serviceUuid = Guid("19B10000-E8F2-537E-4F6C-D104768A1214");
   final Guid characteristicUuid = Guid("19B10001-E8F2-537E-4F6C-D104768A1214");
 
+  final widgetTextColor = const Color(0xffff6f00);
+  final backgroundColor = const Color(0xff0b0b0b);
+  final widgetTextColorTest = const Color(0xffff1744);
+  // tech green : 0xff64dd17
+
   @override
   void initState() {
     super.initState();
@@ -32,7 +38,6 @@ class _ControlScreenState extends State<ControlScreen> {
     ].request();
     return status.values.every((s) => s.isGranted);
   }
-
 
   void _startScan() async {
     var status = await requestBluetoothPermissions();
@@ -57,7 +62,9 @@ class _ControlScreenState extends State<ControlScreen> {
           FlutterBluePlus.stopScan();
           await _connectToDevice(r.device);
           break;
-        } else {print("fail !!!");}
+        } else {
+          print("fail !!!");
+        }
       }
     });
   }
@@ -99,115 +106,298 @@ class _ControlScreenState extends State<ControlScreen> {
     }
   }
 
-  void _turnLeft() async {
+  void _rotateLeft() async {
     if (_characteristic != null) {
-      await _characteristic!.write([0x03]);
+      await _characteristic!.write([0x3]);
     }
   }
 
-  void _turnRight() async {
+  void _rotateRight() async {
     if (_characteristic != null) {
       await _characteristic!.write([0x04]);
     }
   }
 
+  void _turnLeft() async {
+    if (_characteristic != null) {
+      await _characteristic!.write([0x10]);
+    }
+  }
 
+  void _turnRight() async {
+    if (_characteristic != null) {
+      await _characteristic!.write([0x11]);
+    }
+  }
+
+  void _turnLeftBack() async {
+    if (_characteristic != null) {
+      await _characteristic!.write([0x12]);
+    }
+  }
+
+  void _turnRightBack() async {
+    if (_characteristic != null) {
+      await _characteristic!.write([0x13]);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.bottomLeft,
-            end: Alignment.topRight,
-            colors: <Color>[
-              Color(0xff214c91),
-              Color(0xff154494),
-              Color(0xff093c96),
-              Color(0xff033297),
-              Color(0xff062896),
-              Color(0xff111b95),
-              Color(0xff1d149b),
-              Color(0xff2a16aa),
-              Color(0xff3717b9),
-              Color(0xff4317c8),
-              Color(0xff5117d7),
-              Color(0xff5e15e6),
-            ],
-          ),
-        ),
+        color: Colors.black,
+        // decoration: const BoxDecoration(
+        //   gradient: LinearGradient(
+        //     begin: Alignment.bottomLeft,
+        //     end: Alignment.topRight,
+        //     colors: <Color>[
+        //       Color(0xff214c91),
+        //       Color(0xff154494),
+        //       Color(0xff093c96),
+        //       Color(0xff033297),
+        //       Color(0xff062896),
+        //       Color(0xff111b95),
+        //       Color(0xff1d149b),
+        //       Color(0xff2a16aa),
+        //       Color(0xff3717b9),
+        //       Color(0xff4317c8),
+        //       Color(0xff5117d7),
+        //       Color(0xff5e15e6),
+        //     ],
+        //   ),
+        // ),
         child: Row(
           children: <Widget>[
             const SizedBox(
-              width: 90,
+              width: 10,
             ),
             SizedBox(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: backgroundColor,
+                    ),
                     onPressed: _characteristic != null ? _forward : null,
-                    child: const SizedBox(
+                    child: SizedBox(
                       width: 100,
-                      height: 80,
+                      height: 100,
                       child: Center(
-                        child: Text("FORWARD"),
+                        child: Text(
+                          "FORWARD",
+                          style: GoogleFonts.orbitron(
+                            color: widgetTextColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                   ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: backgroundColor,
+                      ),
                       onPressed: _characteristic != null ? _stop : null,
-                      child: const SizedBox(
+                      child: SizedBox(
                         width: 100,
-                        height: 80,
+                        height: 100,
                         child: Center(
-                          child: Text("STOP"),
+                          child: Text(
+                            "STOP",
+                            style: GoogleFonts.orbitron(
+                              color: widgetTextColor,
+                              fontSize: 13,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                            ),
+                          ),
                         ),
                       )),
                   ElevatedButton(
                     onPressed: _characteristic != null ? _backward : null,
-                    child: const SizedBox(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: backgroundColor,
+                    ),
+                    child: SizedBox(
                       width: 100,
-                      height: 80,
+                      height: 100,
                       child: Center(
-                        child: Text("BACKWARD"),
+                        child: Text(
+                          "BACKWARD",
+                          style: GoogleFonts.orbitron(
+                            color: widgetTextColor,
+                            fontSize: 13,
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
                       ),
                     ),
                   ),
                 ],
               ),
             ),
-            ElevatedButton(
-              child: const Text("barrel commands"),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const FiringScreen()));
-              },
+            SizedBox(
+              width: 140,
+              height: 60,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: backgroundColor,
+                ),
+                child: Text(
+                  "barrel commands",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.orbitron(
+                    color: widgetTextColor,
+                    fontSize: 13,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const FiringScreen()));
+                },
+              ),
             ),
             SizedBox(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed:  _characteristic != null ? _turnLeft : null,
-                    child: const SizedBox(
-                      width: 100,
-                      height: 120,
-                      child: Center(
-                        child: Text("TURN LEFT"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: backgroundColor,
+                        ),
+                        onPressed: _characteristic != null ? _rotateLeft : null,
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Transform(
+                              alignment: Alignment.center,
+                              transform: Matrix4.identity()
+                                ..scale(-1.0, 1.0, 1.0),
+                              child: Icon(
+                                Icons.refresh_rounded,
+                                color: widgetTextColor,
+                                size: 50.0,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: backgroundColor,
+                        ),
+                        onPressed:
+                            _characteristic != null ? _rotateRight : null,
+                        child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: Center(
+                            child: Icon(
+                              Icons.refresh_rounded,
+                              color: widgetTextColor,
+                              size: 50.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                  ElevatedButton(
-                    onPressed: _characteristic != null ? _turnRight : null,
-                    child: const SizedBox(
-                      width: 100,
-                      height: 120,
-                      child: Center(
-                        child: Text("TURN RIGHT"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: backgroundColor,
+                        ),
+                        onPressed: _characteristic != null ? _turnLeft : null,
+                        child: SizedBox(
+                          width: 110,
+                          height: 120,
+                          child: Center(
+                            child: Icon(
+                              Icons.undo,
+                              color: widgetTextColor,
+                              size: 50.0,
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: backgroundColor,
+                        ),
+                        onPressed: _characteristic != null ? _turnRight : null,
+                        child: SizedBox(
+                          width: 110,
+                          height: 120,
+                          child: Center(
+                            child: Icon(
+                              Icons.redo,
+                              color: widgetTextColor,
+                              size: 50.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: backgroundColor,
+                        ),
+                        onPressed:
+                            _characteristic != null ? _turnLeftBack : null,
+                        child: SizedBox(
+                          width: 100,
+                          height: 110,
+                          child: Transform(
+                            alignment: Alignment.center,
+                            transform: Matrix4.identity()
+                              ..scale(1.0, -1.0, 1.0),
+                            child: Icon(
+                              Icons.undo,
+                              color: widgetTextColor,
+                              size: 50.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: backgroundColor,
+                        ),
+                        onPressed:
+                            _characteristic != null ? _turnLeftBack : null,
+                        child: SizedBox(
+                          width: 100,
+                          height: 110,
+                          child: Transform(
+                            alignment: Alignment.center,
+                            transform: Matrix4.identity()
+                              ..scale(1.0, -1.0, 1.0),
+                            child: Icon(
+                              Icons.redo,
+                              color: widgetTextColor,
+                              size: 50.0,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
