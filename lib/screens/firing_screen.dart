72e644542a0e7637bb823b341bd2acee90bb61dc
Antoine <antoine.bnrd2@gmail.com>
2025-06-22 19:48:46 +0200

    added of the feature to raise and lower the turret + "fire" button

--- lib/screens/firing_screen.dart
+++ lib/screens/firing_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tank_app/screens/control_screen.dart';
+import 'package:tank_app/theme/styles.dart';
+import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
 class FiringScreen extends StatefulWidget {
   const FiringScreen({super.key});
@@ -9,22 +11,83 @@ class FiringScreen extends StatefulWidget {
 }
 
 class _FiringScreenState extends State<FiringScreen> {
+
+  final elevation = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          const SizedBox(
-            width: 10,
-          ),
-          ElevatedButton(
-            child: const Text("tank control"),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (_) => const ControlScreen()));
-            },
-          )
-        ],
+    return Scaffold(
+      body: Container(
+        color: backgroundColor,
+        child: Row(
+          children: <Widget>[
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: buttonBackgroundColor,
+                  ),
+                  child: Text("tank control", style: buttonTextStyle),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => const ControlScreen()));
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            SleekCircularSlider(
+              min: -40,
+              max: 30,
+              initialValue: 0,
+              appearance: CircularSliderAppearance(
+                infoProperties: InfoProperties(
+                  modifier: (double value) {
+                    value = -1 * value;
+                    return '${value.toStringAsFixed(0)}°';
+                  },
+                ),
+                customWidths: CustomSliderWidths(
+                  handlerSize: 8,
+                  trackWidth: 8,
+                  progressBarWidth: 30,
+                ),
+                customColors: CustomSliderColors(
+                  progressBarColor: Colors.deepOrange,
+                  trackColor: Colors.grey.shade800,
+                  dotColor: Colors.white,
+                  shadowColor: Colors.black,
+                ),
+                angleRange: 70,
+                startAngle: 320,
+                size: 300,
+              ),
+              onChange: (double value) {
+                int elevation = -1 * value.round();
+                print(elevation);
+              },
+            ),
+            Transform.rotate(
+              angle: elevation * 3.1415 / 180,
+              child: const Icon(Icons.expand_less, size: 40, color: widgetTextColor,),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: buttonBackgroundColor,
+              ),
+              onPressed: (null),
+              child: Text("FIRE !", style: buttonTextStyle),
+            ),
+          ],
+        ),
       ),
     );
   }
