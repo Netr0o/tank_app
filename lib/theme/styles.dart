d9225ee3ad5bc5dec75c3cd0b4b6d93ae9cb129b
Antoine <antoine.bnrd2@gmail.com>
2025-06-24 19:13:53 +0200

    Add two colors

--- lib/theme/styles.dart
+++ lib/theme/styles.dart
@@ -4,8 +4,11 @@ import 'package:google_fonts/google_fonts.dart';
 const Color widgetTextColor = Color(0xffff6f00);
 const buttonBackgroundColor = Color(0xff0b0b0b);
 const backgroundColor = Colors.black;
+const angleControlColor = Colors.deepOrange;
 const widgetTextColorTest = Color(0xffff1744);
 // tech green : 0xff64dd17 0xffff6f00
+const test2color = Color(0xfffc2003);
+const techGreen = Color(0xff64dd17);
 
 final TextStyle buttonTextStyle = GoogleFonts.orbitron(
   color: widgetTextColor,
