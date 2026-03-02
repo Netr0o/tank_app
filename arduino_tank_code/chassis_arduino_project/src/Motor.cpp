a7fc533fe101ffbd892638fe2173f57c87507ad2
Antoine <antoine.bnrd2@gmail.com>
2026-03-02 16:14:15 +0100

    ajout de la définitions des méthodes

--- arduino_tank_code/chassis_arduino_project/src/Motor.cpp
+++ arduino_tank_code/chassis_arduino_project/src/Motor.cpp
@@ -18,3 +18,32 @@ void Motor::forward(uint8_t){
     analogWrite(pwmBackward, speed);
     current = 1;
 }
+
+void Motor::backward(uint8_t speed){
+    analogWrite(pwmForward, speed);
+    analogWrite(pwmBackward, 0);
+    current = 2;
+}
+
+void Motor::stop(){
+    progressiveBrake();
+    current = 0;
+}
+
+void Motor::progressiveBrake(uint8_t currentSpeed){
+    if(current = 1){
+        for(int s = currentSpeed; s>=0 ; s-=5){
+            analogWrite(pwmForward, s);
+            analogWrite(pwmBackward, 0);
+            delay(10);
+        }
+    }else if(current = 2){
+        for (int s = currentSpeed; s >= 0 ; s-=5){
+            analogWrite(pwmForward, 0);
+            analogWrite(pwmBackward, s);
+        }
+    }
+    analogWrite(pwmForward, 0);
+    analogWrite(pwmBackward, 0);
+}
+
