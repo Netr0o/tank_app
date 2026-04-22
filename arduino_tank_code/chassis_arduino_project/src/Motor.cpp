d36a70b9834b5d20550f6ebde6fc9eb79808a286
Antoine <antoine.bnrd2@gmail.com>
2026-04-22 23:17:47 +0200

    différenciation de l'état de la dernière commande et de la vitesse en cours

--- arduino_tank_code/chassis_arduino_project/src/Motor.cpp
+++ arduino_tank_code/chassis_arduino_project/src/Motor.cpp
@@ -1,9 +1,7 @@
-#include <cstdint>
 #include "Motor.hpp"
-#include <Arduino.h>
 
 
-Motor::Motor(uint8_t fwd, uint8_t back, uint8_t en1, uint8_t en2)
+Motor::Motor(uint8_t fwd, uint8_t back, uint8_t en1, uint8_t en2) :
     pwmForward(fwd), pwmBackward(back), enable1(en1), enable2(en2){}
 
 void Motor::begin(){
@@ -11,33 +9,39 @@ void Motor::begin(){
     pinMode(pwmBackward, OUTPUT);
     pinMode(enable1, OUTPUT);
     pinMode(enable2, OUTPUT);
+
+    digitalWrite(enable1, HIGH); 
+    digitalWrite(enable2, HIGH);
 }
 
-void Motor::forward(uint8_t){
-    analogWrite(pwmForward, 0);
-    analogWrite(pwmBackward, speed);
-    current = 1;
+void Motor::forward(uint8_t speed){
+    analogWrite(pwmForward, speed);
+    analogWrite(pwmBackward, 0);
+    state = 1;
+    currentSpeed = speed;
 }
 
 void Motor::backward(uint8_t speed){
-    analogWrite(pwmForward, speed);
-    analogWrite(pwmBackward, 0);
-    current = 2;
+    analogWrite(pwmForward, 0);
+    analogWrite(pwmBackward, speed);
+    state = 2;
+    currentSpeed = speed;
 }
 
 void Motor::stop(){
     progressiveBrake();
-    current = 0;
+    state = 0;
+    currentSpeed = 0;
 }
 
-void Motor::progressiveBrake(uint8_t currentSpeed){
-    if(current = 1){
+void Motor::progressiveBrake(){
+    if(state == 1){
         for(int s = currentSpeed; s>=0 ; s-=5){
             analogWrite(pwmForward, s);
             analogWrite(pwmBackward, 0);
             delay(10);
         }
-    }else if(current = 2){
+    }else if(state == 2){
         for (int s = currentSpeed; s >= 0 ; s-=5){
             analogWrite(pwmForward, 0);
             analogWrite(pwmBackward, s);
