5e25b1fc6e70b362c932e654f776d9ca63837ab4
Antoine <antoine.bnrd2@gmail.com>
2026-04-24 11:56:02 +0200

    séparation de la classe BluetoothManager du main vers un fichier spécifique bluetooth_services.dart

---
+++ lib/bluetoothServices/bluetooth_services.dart
@@ -0,0 +1,113 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+class BluetoothManager {
+  static final BluetoothManager _instance = BluetoothManager._internal();
+  factory BluetoothManager() => _instance;
+  BluetoothManager._internal();
+
+  BluetoothDevice? _device;
+  BluetoothCharacteristic? _characteristic;
+
+  final String targetDeviceName = "MOUHAHA";
+  final Guid serviceUuid = Guid("19B10000-E8F2-537E-4F6C-D104768A1214");
+  final Guid characteristicUuid = Guid("19B10001-E8F2-537E-4F6C-D104768A1214");
+
+  void start() {
+    _startScan();
+  }
+
+  Future<bool> requestBluetoothPermissions() async {
+    final status = await [
+      Permission.bluetoothScan,
+      Permission.bluetoothConnect,
+      Permission.location
+    ].request();
+    return status.values.every((s) => s.isGranted);
+  }
+
+  void _startScan() async {
+    var status = await requestBluetoothPermissions();
+
+    if (!status) {
+      print("perm not allowed");
+    }
+
+    bool isOn = await FlutterBluePlus.isOn;
+    if (!isOn) {
+      print("Bluetooth is off. Please enable Bluetooth.");
+      return;
+    }
+
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 5));
+
+    FlutterBluePlus.scanResults.listen((results) async {
+      for (ScanResult r in results) {
+        print("result : ${r.device.name}");
+        if (r.advertisementData.advName == targetDeviceName) {
+          print("found device: ${r.advertisementData.advName}");
+          FlutterBluePlus.stopScan();
+          await _connectToDevice(r.device);
+          break;
+        } else {
+          print("fail !!!");
+        }
+      }
+    });
+  }
+
+  Future<void> _connectToDevice(BluetoothDevice device) async {
+    await device.connect();
+    _device = device;
+
+    List<BluetoothService> services = await device.discoverServices();
+    for (var service in services) {
+      if (service.uuid == serviceUuid) {
+        for (var characteristic in service.characteristics) {
+          if (characteristic.uuid == characteristicUuid) {
+            _characteristic = characteristic;
+            print("found !!!!!");
+            return;
+          }
+        }
+      }
+    }
+  }
+
+
+  void stop() async {
+    await _characteristic?.write([0x00]);
+  }
+
+  void forward() async {
+    await _characteristic?.write([0x01]);
+  }
+
+  void backward() async {
+    await _characteristic?.write([0x02]);
+  }
+
+  void turnLeft() async {
+    await _characteristic?.write([0x03]);
+  }
+
+  void turnRight() async {
+    await _characteristic?.write([0x04]);
+  }
+
+  void rotateLeft() async {
+    await _characteristic?.write([0x0f]);
+  }
+
+  void rotateRight() async {
+    await _characteristic?.write([0x10]);
+  }
+
+  void turnLeftBack() async {
+    await _characteristic?.write([0x11]);
+  }
+
+  void turnRightBack() async {
+    await _characteristic?.write([0x12]);
+  }
+}
